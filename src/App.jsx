04816190f20ee8c2b82beaab634d1f1861b990fe
James Plant <jeplant@email.sc.edu>
2026-03-31 15:28:52 -0400

    installed a bunch of stuff to get the frontend displaying correctly

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Navbar from "./layout/Navbar";
+
 
 function App() {
   const [count, setCount] = useState(0)
